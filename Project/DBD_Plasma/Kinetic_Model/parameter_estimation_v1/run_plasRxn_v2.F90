ffcfb43eddbb4ac4d312a869a1ec08636c48025c
SJBAEK <100sjun91@gmail.com>
2024-11-22 18:37:14 +0900

    241122_Plasma_Kinetic_Solver

--- Project/DBD_Plasma/Kinetic_Model/parameter_estimation_v1/run_plasRxn_v2.F90
+++ Project/DBD_Plasma/Kinetic_Model/parameter_estimation_v1/run_plasRxn_v2.F90
@@ -1,49 +1,61 @@
+
 program run_plasRxn
 !
 ! declare variables and modules
 !
   use ZDPlasKin
   implicit none
-  double precision, parameter :: gas_temperature  = 298.15d0, & ! gas temperature, K
-                                 density_ini_ch4   = 2.46d19,  & ! initial CH4 density, cm-3
-                                 density_ini_elec = 1.0d0,&      ! initial electron density, cm-3
-                                 reduced_field    = 10       ! reduced electric field
-  double precision            :: time = 0.0d0, time_end = 5.0d0, dtime = 1.0d-4,  EN = 0.0d0,& ! times, s // Reduced Electric field, Td
-                                 t1, t2, tc                       ! calculation time
+  double precision, parameter :: gas_pressure      = 1.0d0,    & ! pressure, atm
+                                 gas_temperature   = 400.0d0,   & ! gas temperature, K
+                                 ini_elec_density  = 1.0d6,    & ! initial electron density, cm-3
+                                 ini_gas_density   = 101325.0d0 * gas_pressure & 
+                                                   / gas_temperature / 1.38d-17,  & ! gas density, cm-3
+                                 power             = 11.7,                          & ! Power, W
+                                 volume            = 1.05                           ! plasma volume, cm3
+  double precision            :: time, time_end, dtime, EN,& ! times, s // Reduced Electric field, Td
+                                 t1, t2, tc, mo, all_neutral                       ! calculation time, mobility
   integer                     :: i
 !
 ! print
 !
   write(*,'(/,A)') 'CH4 DBD Plasma Reactor'
 ! Initialization of the program
-    call CPU_TIME(t1)           ! to get the processing time at the end
+    call CPU_TIME(t1)           ! to get the processing time at the end!
+    call ZDPlasKin_init()
 !
-! initialization of ZDPlasKin
+! set the physical conditions of the calculation
 !
-  call ZDPlasKin_init()
-!
-! set the physical conditions of the calculation:
-!     the gas temperature and the reduced electric field
-!
-  call ZDPlasKin_set_conditions(GAS_TEMPERATURE=gas_temperature,REDUCED_FIELD=reduced_field)
-!
-! set initial densities
-!
-  call ZDPlasKin_set_density(  'CH4',density_ini_ch4)
-  call ZDPlasKin_set_density(    'e',density_ini_elec)
-  call ZDPlasKin_set_density('CH4^+',density_ini_elec)
+
+  call ZDPlasKin_set_conditions(GAS_TEMPERATURE=gas_temperature)
+  call ZDPlasKin_set_density(  'CH4',ini_gas_density)
+  call ZDPlasKin_set_density(    'e',ini_elec_density)
+  call ZDPlasKin_set_density('CH4^+',ini_elec_density)
   call ZDPlasKin_set_config(QTPLASKIN_SAVE=.true.)
 !
-! print column headers and initial values
+! initialization of variables
+    time = 0.0d0
+    time_end = 2.0d-2
+    dtime = 1e-8
+    EN = sqrt(power/ini_elec_density/(1.6022d-19)/3.5552d23*ini_gas_density/volume)/ini_gas_density/(1d-17)
+
+! Tells Bolsig the value of the reduced field (to do each time EN changes)
+    call ZDPlasKin_set_conditions(REDUCED_FIELD=EN)
 !
-  write(*,'(4(A12))') 'Time_s', ( trim(species_name(i)), i = 1, species_max )
-  write(*,'(4(1pe12.4))') time, density(:)
+! write the headers of the output file
+    write(*,'(99(A14))') 'Time_s', 'dtime_s', 'EN_Td', 'Elec_cm-3'
+    write(1,'(99(A14))', advance='no') 'Time', 'dTime', 'E/N', (trim(species_name(i)), i = 1, species_max)
 !
 ! time integration
 !
+  WRITE(*, '(A)') "Calculation Start!!"
   do while(time .lt. time_end)
+    call ZDPlasKin_set_conditions(REDUCED_FIELD=EN)
     call ZDPlasKin_timestep(time,dtime)
     time = time + dtime
+    call ZDPlasKin_get_conditions(ELEC_MOBILITY_N=mo)
+    call ZDPlasKin_get_density_total(ALL_NEUTRAL = all_neutral)
+    EN = sqrt(power/density(species_electrons)/(1.6022d-19)/mo*all_neutral/volume)/all_neutral/(1d-17)
+
     write(*,'(4(1pe12.4))') time
   enddo
 !
